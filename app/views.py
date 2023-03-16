99b38a32d7f10ab05e12dc301b88482d0fd9ed89
Shahathaj <bujjima710@gmail.com>
2023-03-16 12:22:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bujji(request):
+    return render(request,'bujji.html')
+
+def shahathaj(request):
+    return render(request,'shahathaj.html')
